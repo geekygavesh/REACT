bd948824221914719d669f364ec7e0fc39b97b6e
Gavesh Tomer <gtomer357@gmail.com>
2024-10-23 01:44:20 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1",{id : "heading"},"Hello Dosto");
 // console.log(heading);
 
@@ -9,8 +10,8 @@
 // nested  with siblings
 const parent = React.createElement("div",{id:"parent"},[
     React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I'm h1 tag"),
-    React.createElement("h2",{},"I'm h2 tag")
+    React.createElement("h1",{},"Hello from namaste"),
+    React.createElement("h2",{},"I'm broo")
     ]),
     React.createElement("div",{id:"child2"},[
     React.createElement("h1",{},"I'm h1 tag"),
---
+++ README.md
@@ -0,0 +1,19 @@
+# Namaste React🚀
+
+# Parcel
+- Dev build
+- Local Server
+- HMR = Hot module replacement
+- File Watching Algorithm
+- Caching - Faster Builds
+- Image Optimisation
+- Minification of File
+- Bundling
+- Compress
+- Content Hashing
+- Code Splitting
+- Differential Bundling -> support older browsers
+- Diagnostics
+- Error Handling
+- Tree Shaking -> Remove unused code
+- Diff dev and prod bundles
